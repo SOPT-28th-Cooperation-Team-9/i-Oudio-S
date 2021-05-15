78b9d27dac8678642b57d6d40cf7a1d3b499c26d
hongjinseok <sldkdl111@ajou.ac.kr>
2021-05-16 05:58:22 +0900

    UI setting

--- i-Oudio-S/ViewController.swift
+++ i-Oudio-S/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btn(_ sender: Any) {
+    
+        let storyboard = UIStoryboard.init(name: "audio", bundle : nil)
+        guard let nextVC = storyboard.instantiateViewController(identifier: "HomeVC") as? HomeVC else{return}
+        self.navigationController?.pushViewController(nextVC, animated: true)
+        nextVC.modalPresentationStyle = .overFullScreen
+        self.present(nextVC, animated: true, completion: nil)
+    }
+    
 }
 
