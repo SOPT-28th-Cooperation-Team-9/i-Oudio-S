8e2737dfae696e882ff356f1ac6a03bcb0ad0120
hryeong66 <jhr110326@gmail.com>
2021-05-21 11:24:11 +0900

    [ADD] SNS부분 tableCell, collectioCell추가

---
+++ i-Oudio-S/Source/HeyryeongScene/View/Cell/SNSBookCVC.swift
@@ -0,0 +1,17 @@
+//
+//  SNSBookCVC.swift
+//  i-Oudio-S
+//
+//  Created by 장혜령 on 2021/05/18.
+//
+
+import UIKit
+
+class SNSBookCVC: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ i-Oudio-S/Source/HeyryeongScene/View/Cell/SNSBookTVC.swift
@@ -0,0 +1,23 @@
+//
+//  SNSBookTVC.swift
+//  i-Oudio-S
+//
+//  Created by 장혜령 on 2021/05/18.
+//
+
+import UIKit
+
+class SNSBookTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
